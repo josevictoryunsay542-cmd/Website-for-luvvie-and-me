6f366913d46ad131ecc7fc4918cb892a17d648ba
josevictoryunsay542-cmd <josevictoryunsay542@gmail.com>
2026-08-30 16:14:19 +0800

    Update planner.js

--- planner.js
+++ planner.js
@@ -574,3 +574,14 @@ window.addEventListener(
 
     }
 );
+
+
+function openSidebar() {
+    document.getElementById("sidebar").classList.add("open");
+    document.getElementById("sidebarOverlay").classList.add("open");
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").classList.remove("open");
+    document.getElementById("sidebarOverlay").classList.remove("open");
+}
