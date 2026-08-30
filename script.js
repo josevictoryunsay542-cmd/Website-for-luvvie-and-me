f440e51fa9b50765af5ebf027a63c8851aee8c3d
josevictoryunsay542-cmd <josevictoryunsay542@gmail.com>
2026-08-30 16:02:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -111,3 +111,12 @@ function resetJournal() {
 
 }
 
+function openSidebar() {
+    document.getElementById("sidebar").classList.add("open");
+    document.getElementById("sidebarOverlay").classList.add("open");
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").classList.remove("open");
+    document.getElementById("sidebarOverlay").classList.remove("open");
+}
